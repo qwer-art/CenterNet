6f5d9ed8daa1a33da4d79a336469c72362a8ae95
liyun <1062428799@qq.com>
2024-11-17 12:37:13 +0800

    归一化的图片处理完成

--- utils/torch_dataset.py
+++ utils/torch_dataset.py
@@ -23,9 +23,11 @@ def test_transform():
     width, height = image.size
     print(f"pil_image: ({height}x{width}),min: {np.min(image)},max: {np.max(image)}")
     transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),  # 随机水平翻转
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ToTensor()
     ])
     # 2.tf_image
     transformed_image = transform(image)
